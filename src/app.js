ed60f2317226bb8647d61cabd56c085d73c9f2f2
NIKITAPRO965 <nikita.pro.965@gmail.com>
2026-02-20 12:55:53 +0200

    .

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import add from "./js/calculator"
+
+console.log(add(2, 3));
+
+
+import {calculator, math} from "./js/calculator"
+
+console.log(calculator(3, 7));
+console.log(math(966, 1));
+
+
+
+
+
+// import copy from "./js/calculator"
+// console.log(copy);
+// console.log(copy.calc(12, 13));
+// console.log(copy.m(1000, 500));
+
---
+++ src/js/calculator.js
@@ -0,0 +1,27 @@
+export default function calculator(a, b) {
+    return a + b
+}
+
+
+
+export function calculator(a, b) {
+    return a + b
+}
+
+export function math(a, b) {
+    return a - b
+}
+
+
+
+
+
+
+// function calc(a, b) {
+//     return a + b
+// }
+
+// function m(a, b) {
+//     return a - b
+// }
+// export default {calc, m}
